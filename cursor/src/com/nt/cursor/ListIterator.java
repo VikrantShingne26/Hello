a7c94aa3db12cbb3784c0ec8490dc68dedd85b71
Vikrant Shingne <vkky.shingne@gmail.com>
2016-12-21 01:38:46 +0530

    Hello ListIterator

---
+++ cursor/src/com/nt/cursor/ListIterator.java
@@ -0,0 +1,18 @@
+package com.nt.cursor;
+
+import java.util.LinkedList;
+
+public class ListIterator {
+	public static void main(String[] args) {
+LinkedList ll=new LinkedList();
+ll.add("raja");
+ll.add("rahul");
+ll.add("ravi");
+System.out.println(ll);
+
+	}
+	
+}
+
+
+
